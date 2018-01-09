5ac720e4a1edfa004eb542693001ad79d9c69339
张峰 <zhangfeng@hujiang.com>
2018-01-09 22:59:43 +0800

    update: 整理 greeting 页

--- pages/greeting/greeting.js
+++ pages/greeting/greeting.js
@@ -1,26 +1,17 @@
 import { fade } from '../../modules/pageTransitionAnimation.js';
 import { PAGE_TRANSITION_ANIMATION_DURATION } from '../../constants/config.js';
 
-// pages/index/index.js
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     pageTransitionData: {}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
-    // 注册跳转定时器
     this.timer = setTimeout(() => {
       this.redirectToIndex();
     }, 8000);
-
   },
 
   handleSightTap() {
@@ -31,8 +22,7 @@ Page({
   redirectToIndex() {
     this.setData({ pageTransitionData: fade.out().export() });
     setTimeout(() => {
-      console.log('redirect');
-      // this.redirectTo('/pages/index/index');
+      wx.navigateTo({ url: '/pages/index/index' });
     }, PAGE_TRANSITION_ANIMATION_DURATION);
   }
 });
